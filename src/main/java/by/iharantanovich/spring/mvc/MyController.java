d831f80d9c20cba6f17c55dde63f87d3861c1bd6
garikAntonovich <garik.antonovich@gmail.com>
2021-11-16 01:28:56 +0300

    usage model

--- src/main/java/by/iharantanovich/spring/mvc/MyController.java
+++ src/main/java/by/iharantanovich/spring/mvc/MyController.java
@@ -1,7 +1,10 @@
 package by.iharantanovich.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,8 +19,19 @@ public class MyController {
         return "ask-emp-detail-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails() {
+//        return "show-emp-detail-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails() {
+    public String showEmployeeDetails(HttpServletRequest request, Model model) {
+
+        String empName = request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+        model.addAttribute("description", " - sad man");
+
         return "show-emp-detail-view";
     }
 }
